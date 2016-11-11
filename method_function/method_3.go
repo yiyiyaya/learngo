163fe7f23fc126b239aa455dd8e4be3a3f585e75
yiyiyaya <15712974451@163.com>
2016-11-11 17:20:01 +0800

    method

---
+++ method_function/method_3.go
@@ -0,0 +1,29 @@
+package main
+
+import ("fmt"
+         "math")
+
+type Point struct{ x, y float64 }
+
+type Path []Point
+func(p Point)Distance(q Point)float64{
+    return math.Hypot(q.x-p.x,q.y-p.y)
+}
+
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
+func main() {
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{1, 1}}
+	fmt.Println(perim.Distance())
+}
